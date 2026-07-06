91c9724a4c5b4d6c60969223337ca7c5ded126e7
EchoSave <echo.salonga@edu.sait.ca>
2026-07-06 12:48:29 -0600

    Start of Auth form

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
+import NavBar from "./componets/NavBar";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+// export default function RootLayout() {
+//   return <Stack ><NavBar/></Stack>;
+// }
