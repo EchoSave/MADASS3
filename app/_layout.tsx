8e5526d7284c786790d4992c9c6a5c75946d5743
Saron <Saron.okbamichael@edu.sait.ca>
2026-07-24 23:11:25 -0400

    Correct navigation structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,4 @@
 import { Stack } from "expo-router";
-<<<<<<< HEAD
-import React from "react";
 import { AuthProvider } from "./context/AuthContext";
 
 export default function RootLayout() {
@@ -11,14 +9,5 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" />
       </Stack>
     </AuthProvider>
-=======
-export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      
-      <Stack.Screen name="(auth)"/>
-      <Stack.Screen name="(tabs)"/>
-    </Stack>
->>>>>>> c6ddfafb8bbc0b2c083eac834567a71eb3537558
   );
 }
